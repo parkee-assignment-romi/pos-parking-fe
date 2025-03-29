363a07b8778b70c434610966ec9efcb8bebf1b52
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2025-03-29 15:41:29 +0700

    feat: integrate with check in and check out page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import CheckInPage from './pages/CheckInPage';
+import CheckOutPage from './pages/CheckOutPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav>
+          <Link to="/checkin">Check-In</Link>
+          <Link to="/checkout">Check-Out</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<CheckInPage />} />
+          <Route path="/checkin" element={<CheckInPage />} />
+          <Route path="/checkout" element={<CheckOutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
